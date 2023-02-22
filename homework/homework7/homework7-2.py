ca4458231f215c2833aa9e135f6f8423c33679e8
LosRR <losrr@mail.ru>
2023-02-22 23:01:41 +0300

    выполнена 7 домашняя работа

--- homework/homework7/homework7-2.py
+++ homework/homework7/homework7-2.py
@@ -5,9 +5,9 @@
 #почему не с нуля). Примечание: бинарной операцией называется любая операция, у которой
 #ровно два аргумента, как, например, у операции умножения.
 
-def printOperationTable(operation, numRows=6, numColumns=6):
-    for row in range(1, numRows+1):
-        for column in range(1, numColumns+1):
+def print_operation_table(operation, num_rows=6, num_columns=6):
+    for row in range(1, num_rows+1):
+        for column in range(1, num_columns+1):
             print(operation(row,column), end='\t')
         print()
-printOperationTable(lambda x,y: x*y)
+print_operation_table(lambda x,y: x*y)
